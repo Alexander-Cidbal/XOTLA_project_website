0573c5febf54d93270ad21d6afd0ec9454d4f05d
Alexander-Cidbal <45251082+Alexander-Cidbal@users.noreply.github.com>
2026-07-09 13:05:21 -0600

    Consolidate action buttons into single Actions column
    Refactor the Downloads table to combine Download and Flash actions into a single Actions column. Replace text buttons with styled icon buttons featuring tooltips, SVG icons, and hover animations. Add custom CSS for button styling with smooth transitions and color-coded hover states (green for download, pink for flash).

--- SupaClient.js
+++ SupaClient.js
@@ -62,20 +62,38 @@
                         <td>${item.created_at ?? '-'}</td>
                         <td>
                             ${item.Link
-                        ? `<a href="${item.Link}" target="_blank" rel="noopener noreferrer" class="btn btn-outline btn-primary btn-xs">
+                        ? `<ul class="wrapper_btns">
+                              <li class="icon download">
+                                <a href="${item.Link}" target="_blank" rel="noopener noreferrer" class="flex items-center justify-center w-full h-full text-current">
+                                  <span class="tooltip">
                                     <span class="lang-en">Download</span>
                                     <span class="lang-es">Descargar</span>
-                                   </a>`
-
-                        : '-'}
-                        </td>
-                        <td>
-                            ${item.Link
-                        ? `<a href="Flashing.html" class="btn btn-outline btn-primary btn-xs">
-                                    <span class="lang-en">Flasher</span>
+                                  </span>
+                                  <svg
+                                    viewBox="0 0 256 256"
+                                    height="32"
+                                    width="38"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M74.34 85.66a8 8 0 0 1 11.32-11.32L120 108.69V24a8 8 0 0 1 16 0v84.69l34.34-34.35a8 8 0 0 1 11.32 11.32l-48 48a8 8 0 0 1-11.32 0ZM240 136v64a16 16 0 0 1-16 16H32a16 16 0 0 1-16-16v-64a16 16 0 0 1 16-16h52.4a4 4 0 0 1 2.83 1.17L111 145a24 24 0 0 0 34 0l23.8-23.8a4 4 0 0 1 2.8-1.2H224a16 16 0 0 1 16 16m-40 32a12 12 0 1 0-12 12a12 12 0 0 0 12-12"
+                                      fill="currentColor"
+                                    ></path>
+                                  </svg>
+                                </a>
+                              </li>
+                              <li class="icon flash">
+                                <a href="Flashing.html" class="flex items-center justify-center w-full h-full text-current">
+                                  <span class="tooltip">
+                                    <span class="lang-en">Flash</span>
                                     <span class="lang-es">Flasher</span>
-                                   </a>`
-
+                                  </span>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-usb-plug-fill" viewBox="0 0 16 16">
+                                    <path d="M6 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4H6zM7 1v1h1V1zm2 0v1h1V1zM5.5 5a.5.5 0 0 0-.5.5v4.894a2 2 0 0 0 .336 1.11l.83 1.245c.544.816.834 1.774.834 2.754 0 .275.222.497.497.497h2.006a.497.497 0 0 0 .497-.497c0-.98.29-1.938.834-2.754l.83-1.245a2 2 0 0 0 .336-1.11V5.5a.5.5 0 0 0-.5-.5z"/>
+                                  </svg>
+                                </a>
+                              </li>
+                           </ul>`
                         : '-'}
                         </td>
                     </tr>
